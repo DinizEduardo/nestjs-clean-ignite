fc263c8ad3bff7e987a4e378b52b48c9e57e639e
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-31 17:48:56 -0300

    feat: usando configmodule para .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: false,
   })
-  await app.listen(3333)
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+  await app.listen(port)
 }
 bootstrap()
