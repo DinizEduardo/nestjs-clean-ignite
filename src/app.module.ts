8164422c7786c8e2d396469ed55f74c2c9f1b4fa
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-09-06 21:39:17 -0300

    feat: controller de listagem de perguntas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate-controller'
 import { CreateAccountController } from './controllers/create-account'
 import { CreateQuestionController } from './controllers/create-question-controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions-controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
@@ -19,6 +20,7 @@ import { PrismaService } from './prisma/prisma.service'
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
